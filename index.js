f4825f6f87836135191d33000892115cbf578479
Nicolas Wanner <cutslade@hotmail.com>
2017-09-13 09:52:11 +0200

    patch

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const matter = require('gray-matter');
 var fs = require('fs')
 var file = fs.readFileSync('_posts/blog/2017-09-12-hello.md', 'utf8');
 
-console.log(file);
+let data = matter(file);
+console.log(data);
 
-//console.log(matter('---\ntitle: Front Matter\n---\nThis is content.'));
-console.log(matter(file));
-
+fs.writeFileSync('./output.html', data);
