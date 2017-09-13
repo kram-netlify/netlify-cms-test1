3a3a05550fa572c2c18f9904f1ec438bbc77743a
Nicolas Wanner <cutslade@hotmail.com>
2017-09-13 09:49:12 +0200

    patch

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('only testing');
+const matter = require('gray-matter');
+var fs = require('fs')
+var file = fs.readFileSync('_posts/blog/2017-09-12-hello.md', 'utf8');
+
+console.log(file);
+
+//console.log(matter('---\ntitle: Front Matter\n---\nThis is content.'));
+console.log(matter(file));
+
